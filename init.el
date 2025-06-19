e37373796db4a0e4eb6cf8011a6cc34d9819977f
ulicitrinitas <uli.citrinitas@gmail.com>
2025-06-19 19:56:21 -0300

    adicionado pacote de icones

--- init.el
+++ init.el
@@ -8,7 +8,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(auto-package-update company telephone-line)))
+ '(package-selected-packages
+   '(all-the-icons auto-package-update company company-box telephone-line)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
